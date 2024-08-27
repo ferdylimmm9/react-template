584cc6ad47d3c0ed44b438d92d2286968603862f
Ferdy Lim <ferdy.lim@springkraf.com>
2024-08-27 16:32:45 +0700

    feat: non-private-route controller

--- src/constants/route/route.tsx
+++ src/constants/route/route.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import RouteEnum from './enum';
 import PrivateRoutes from './private-routes';
 import Homepage from '../../modules/homepage';
+import NonPrivateRoutes from './non-private-routes';
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
   {
     element: <PrivateRoutes />,
     children: [], // register your private routes overhere
+  },
+  {
+    element: <NonPrivateRoutes />,
+    children: [],
   },
 ]);
 
